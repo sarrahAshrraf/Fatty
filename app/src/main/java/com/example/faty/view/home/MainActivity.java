d42669f3bf92515189ab50d117a3bec07cb5c3f0
sarrahAshrraf <sarahsoou01@gmail.com>
2024-02-09 00:14:33 +0200

    done GUI for home fragment

--- app/src/main/java/com/example/faty/view/home/MainActivity.java
+++ app/src/main/java/com/example/faty/view/home/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.faty.view.home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 import com.example.faty.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigationView;
+    NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        bottomNavigationView = findViewById(R.id.btm_nav);
+        navController =Navigation.findNavController(this, R.id.host_frag);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+
+
     }
 }
